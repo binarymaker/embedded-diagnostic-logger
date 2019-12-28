b2f19c0b9a4e0bab6192a5cec4537293fd160426
binarymaker <importfunfromcode@gmail.com>
2019-12-28 13:05:12 +0530

    ceedling unit test init

---
+++ test/unit-test/test_logger/test/test_log.c
@@ -0,0 +1,51 @@
+/**
+  ******************************************************************************
+  * ______  _                             ___  ___        _               
+  * | ___ \(_)                            |  \/  |       | |              
+  * | |_/ / _  _ __    __ _  _ __  _   _  | .  . |  __ _ | | __ ___  _ __ 
+  * | ___ \| || '_ \  / _` || '__|| | | | | |\/| | / _` || |/ // _ \| '__|
+  * | |_/ /| || | | || (_| || |   | |_| | | |  | || (_| ||   <|  __/| |   
+  * \____/ |_||_| |_| \__,_||_|    \__, | \_|  |_/ \__,_||_|\_\\___||_|   
+  *                                 __/ |                                 
+  *                                |___/                                  
+  *                                                                       
+  * Copyright (C) 2019 Binary Maker - All Rights Reserved
+  *
+  * This program and the accompanying materials are made available
+  * under the terms described in the LICENSE file which accompanies
+  * this distribution.
+  * Written by Binary Maker <https://github.com/binarymaker>
+  ******************************************************************************
+  */
+
+#include "unity.h"
+
+
+void
+setUp()
+{
+
+}
+
+void
+tearDown()
+{
+
+}
+
+/* -------------------------------------------------------------------------- */
+void
+test_PORTB_pin_direction_input()
+{
+
+    /* Ensure known test state */
+
+    /* Setup expected function mock */
+
+    /* Function under test */
+
+    /* Verify test results */
+    TEST_ASSERT_EQUAL_INT8( 1, 1);
+
+}
+
